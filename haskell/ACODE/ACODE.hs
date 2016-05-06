5b5ff6c50f7824c3ff00465b820d15dae473c559
Shane Xu <xusheng0711@gmail.com>
2016-05-06 11:01:03 +0800

    去掉hlint warning

--- haskell/ACODE/ACODE.hs
+++ haskell/ACODE/ACODE.hs
@@ -7,10 +7,10 @@ acode xs =
   let pairs = init xs `zip` tail xs
       cal (n1, n2) (x1, x2) =
         ((if x2 /= '0' then n1 else 0) + (if (x1 == '2' && x2 > '6') || (x1 > '2') || (x1 == '0') then 0 else n2), n1)
-  in fst (foldl' cal (1, 1) pairs)
+  in fst $ foldl' cal (1, 1) pairs
 
 main :: IO ()
 main = getContents >>= putStrLn . join . results . init . lines
   where results = map (show . acode)
-        join = (intercalate "\n")
+        join = intercalate "\n"
 
