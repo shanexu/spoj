58178667a23bcd169a0a4d5f8354458eab817d1f
Shane Xu <xusheng0711@gmail.com>
2016-05-16 12:05:37 +0800

    缩减两个字符

--- haskell/KAMIL/KAMIL.hs
+++ haskell/KAMIL/KAMIL.hs
@@ -1,1 +1,1 @@
-main=getContents>>=putStr.unlines.map(show.product.map(const 2).filter(`elem`"LFDT")).lines
+main=getContents>>=putStr.unlines.map(show.product.map(\_->2).filter(`elem`"LFDT")).lines
