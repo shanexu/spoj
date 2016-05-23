76c7a56f8e47907d7c0cd0e461bbdb751679bcab
Shane Xu <xusheng0711@gmail.com>
2016-05-23 22:37:35 +0800

    改进时间

--- haskell/CODE1/CODE1.hs
+++ haskell/CODE1/CODE1.hs
@@ -1,13 +1,16 @@
+{-# LANGUAGE BangPatterns #-}
+{-# OPTIONS_GHC -optc-O3 #-}
+
 import Data.List (intercalate, foldl')
 import Control.Monad
 import Prelude hiding (Rational)
 
-data Rational = Int :/ Int deriving (Show)
+data Rational = !Int :/ !Int deriving (Show)
 
 instance Eq Rational where
   (x:/y) == (x':/y') = x*y' == y*x'
 
-data Complex = Rational :+ Rational deriving (Show)
+data Complex = !Rational :+ !Rational deriving (Show)
 
 instance Eq Complex where
   (x:+y) == (x':+y') = x == x' && y == y'
@@ -49,13 +52,13 @@ go (0:+0) _ _ res _ = Just res
 go x b ma res t
   | t == 100 = Nothing
   | otherwise = foldl' (\r i ->
-                          let x'@((_:/m) :+ (_:/n)) = (x - (fromIntegral i)) / b
+                          let !x'@((_:/m) :+ (_:/n)) = (x - (fromIntegral i)) / b
                           in r `mplus` (if m == 1 && n == 1 then go x' b ma (i:res) (t+1) else Nothing)) Nothing  [0 .. ma]
 
 format :: Maybe [Int] -> String
 format Nothing = "The code cannot be decrypted."
 format (Just []) = "0"
-format (Just as@(_:_)) = intercalate "," (map show as)
+format (Just as) = intercalate "," (map show as)
 
 main :: IO ()
 main = getContents >>= putStr . unlines . map (format . uncurry solve . (\[a,b,c,d] -> ((fromIntegral a) :+ (fromIntegral b), (fromIntegral c) :+ (fromIntegral d))) . map read . words) . tail . lines
