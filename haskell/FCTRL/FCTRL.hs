1457fb1020cbaf5766219d1e24270980cb4f0ec2
Shane Xu <xusheng0711@gmail.com>
2016-05-25 10:11:26 +0800

    去掉注释

--- haskell/FCTRL/FCTRL.hs
+++ haskell/FCTRL/FCTRL.hs
@@ -3,8 +3,8 @@
 import Data.List(foldl')
 import Data.Char
 import qualified Data.ByteString.Unsafe     as BU
-import qualified Data.ByteString.Char8      as S -- 'Strict'
-import qualified Data.ByteString.Lazy.Char8 as L -- 'Lazy'
+import qualified Data.ByteString.Char8      as S
+import qualified Data.ByteString.Lazy.Char8 as L
 
 zero :: Int -> Int
 zero 0 = 0
@@ -16,19 +16,11 @@ zero !n = go n 0
 
 main :: IO ()
 main = do
-  ss <- L.getContents -- done with IO now.
+  ss <- L.getContents
   let (_,ls) = L.break (=='\n') ss
-      -- don't need count, we're allocating lazily
-      file   = L.toChunks (L.tail ls) -- a lazy list of strict cache chunks
-      res = process [] file
-      strs = foldl' (\s n -> show n : s) [] res
-    in putStr . unlines $ strs
+      file   = L.toChunks (L.tail ls)
+  putStr . unlines $ foldl' (\s n -> show n : s) [] (process [] file)
 
--- Optimised parsing of strict bytestrings representing \n separated numbers.
---
--- we have the file as a list of cache chunks align them on \n boundaries, and
--- process each chunk separately when the next chunk is demanded, it will be
--- read in.
 process :: [Int] -> [S.ByteString] -> [Int]
 process i []      = i
 process !i (s:t:ts)
@@ -39,7 +31,6 @@ process !i (s:t:ts)
     ts'     = S.concat [r,r',S.singleton '\n'] : BU.unsafeTail rs : ts
 process i (s: ss) = process (add i s) ss
 
--- process a single cache-sized chunk of numbers, \n aligned
 add :: [Int] -> S.ByteString -> [Int]
 add i s = fst $ S.foldl' f (i, 0) s
   where f (!a, !n) '\n' = (zero n :a, 0)
