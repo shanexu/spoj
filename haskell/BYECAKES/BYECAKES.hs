3fd6068150e985c2f5c2fe6bec5242984c0e33fa
Shane Xu <xusheng0711@gmail.com>
2016-05-27 17:59:51 +0800

    增加BYECAKES

---
+++ haskell/BYECAKES/BYECAKES.hs
@@ -0,0 +1,12 @@
+problem :: String -> ([Int], [Int])
+problem = splitAt 4 . map read . words
+
+solve :: [Int] -> [Int] -> [Int]
+solve s s' = let m = maximum $ zipWith (\x x' -> ceiling (fromIntegral x / fromIntegral x')) s s'
+             in zipWith (\x x' -> m * x' - x) s s'
+
+format :: [Int] -> String
+format = unwords . map show
+
+main :: IO ()
+main = interact $ unlines . map (format . uncurry solve . problem) . init . lines
